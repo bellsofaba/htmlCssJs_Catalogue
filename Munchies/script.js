6f72f5a564e6f6fb49b0abd3dd00ff0d89ba7601
bellsofaba <bellsofaba@gmail.com>
2023-09-12 03:27:04 -0400

    Munchies

--- Munchies/script.js
+++ Munchies/script.js
@@ -1,5 +1,11 @@
+document.querySelector('.menu').addEventListener('click',()=> {
+  document.querySelectorAll('.target').forEach((item)=> {
+    item.classList.toggle('change')
+  })
+})
+
 const icons = document.querySelectorAll('.section-1-icons i')
-let i = 
+let i = 1
 
 setInterval(() => {
   i++
